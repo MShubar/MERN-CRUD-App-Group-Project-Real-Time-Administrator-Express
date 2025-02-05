a9b2512f2bd7b01cc4acb7a8d373a310990f070e
MShubar <pokeeeeepow@gmail.com>
2025-02-05 18:03:13 +0300

    start the project

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const session = require('express-session')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const PORT = process.env.PORT
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(methodOverride('_method'))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: true
+  })
+)
+//routers usage
+app.set('view engine', 'ejs')
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+  console.log(`Connection to MongoDB ${mongoose.connection.name}`)
+})
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
