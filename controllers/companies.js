9a626575dfb416d4c4dc58eabdc7464cc507d3be
MShubar <pokeeeeepow@gmail.com>
2025-02-06 11:03:17 +0300

    done companies and users

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,19 +1,38 @@
-const Company = require('./models/Company')
+const Company = require("../models/Company")
+const { signToken } = require("../middleware/jwt")
+const { createUser } = require("./users")
 
-const create = async (req, res) => {}
+const signUp = async (req, res) => {
+  try {
+    const { name, address, crNumber, phone, size, logoImage, crDocument } =
+      req.body
+    if (!name || !crNumber) {
+      return res.status(400).json({ error: "Missing required fields." })
+    }
 
-const index = async (req, res) => {}
+    const newCompany = await Company.create({
+      name,
+      address,
+      crNumber,
+      phone,
+      size,
+      logoImage,
+      crDocument,
+      userId: req.user._id,
+      status: "CR-in-progress",
+    })
 
-const show = async (req, res) => {}
-
-const update = async (req, res) => {}
-
-const remove = async (req, res) => {}
-
-module.exports = {
-  create,
-  index,
-  show,
-  update,
-  remove
+    const token = signToken(req.user)
+    return res.status(201).json({
+      message: "Company created successfully.",
+      token,
+      company: newCompany,
+      user: req.user,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "Something went wrong!" })
+  }
 }
+
+module.exports = { signUp }
--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,32 @@
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
-const { signToken } = require('../middleware/jwt')
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+const { signToken } = require("../middleware/jwt")
 
-const signUp = async (req, res) => {
+const createUser = async (req, res, next) => {
   try {
-    const { email, password } = req.body
-    // if (!email || !password) {
-    //   return res.status(400).json({ error: 'Missing required fields.' })
-    // }
-    // const userExist = await User.findOne({ email })
-    // if (userExist) {
-    //   return res.status(409).json({ error: 'Email already taken.' })
-    // }
+    const { email, password, role = "company" } = req.body
+    if (!email || !password) {
+      return res.status(400).json({ error: "Missing required fields." })
+    }
+
+    const userExist = await User.findOne({ email })
+    if (userExist) {
+      return res.status(409).json({ error: "Email already taken." })
+    }
+
     const hashedPassword = bcrypt.hashSync(password, +process.env.SALT)
 
-    const user = await User.create({
+    const newUser = await User.create({
       email,
       password: hashedPassword,
-      role: 'company'
+      role,
     })
 
-    const token = signToken(user)
-    return res
-      .status(201)
-      .json({ message: 'User created successfully.', token })
+    req.user = newUser
+    next()
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: 'Something went wrong!' })
+    res.status(500).json({ error: "Something went wrong!" })
   }
 }
 
@@ -34,39 +34,39 @@ const signIn = async (req, res) => {
   try {
     const { email, password } = req.body
     if (!email || !password) {
-      return res.status(400).json({ error: 'Missing required fields.' })
+      return res.status(400).json({ error: "Missing required fields." })
     }
+
     const user = await User.findOne({ email })
     if (!user) {
-      return res.status(400).json({ error: 'Invalid email or password.' })
+      return res.status(400).json({ error: "Invalid email or password." })
     }
+
     const matched = bcrypt.compareSync(password, user.password)
     if (!matched) {
-      return res.status(400).json({ error: 'Invalid email or password.' })
+      return res.status(400).json({ error: "Invalid email or password." })
     }
+
     const token = signToken(user)
-    return res.status(200).json({ message: 'Login successful.', token, user })
+    return res.status(200).json({ message: "Login successful.", token, user })
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: 'Something went wrong!' })
+    res.status(500).json({ error: "Something went wrong!" })
   }
 }
 
 const profile = async (req, res) => {
   if (!req.user || !req.user._id) {
-    return res.status(400).json({ error: 'No user found or token is invalid.' })
+    return res.status(400).json({ error: "No user found or token is invalid." })
   }
   try {
     const user = await User.findById(req.user._id)
-    if (!user) return res.status(404).json({ error: 'User not found.' })
+    if (!user) return res.status(404).json({ error: "User not found." })
     return res.status(200).json(user)
   } catch (error) {
-    console.error('Error in profile route:', error)
-    res.status(500).json({ error: 'Something went wrong!' })
+    console.error("Error in profile route:", error)
+    res.status(500).json({ error: "Something went wrong!" })
   }
 }
-module.exports = {
-  signUp,
-  signIn,
-  profile
-}
+
+module.exports = { createUser, signIn, profile }
