8a95b528cce1e9bb2aeac9d06b8cdf2bb1943ab7
MShubar <pokeeeeepow@gmail.com>
2025-02-10 19:51:02 +0300

    Just to pull

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const methodOverride = require('method-override')
 const PORT = process.env.PORT
 
 // require paths
-const userRouter = require("./routes/users")
-const companyRouter = require("./routes/companies")
-const employeeRouter = require("./routes/employees")
-const employeeShiftsRouter = require("./routes/employeeshifts")
+const userRouter = require('./routes/users')
+const companyRouter = require('./routes/companies')
+const employeeRouter = require('./routes/employees')
+const employeeShiftsRouter = require('./routes/employeeshifts')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -35,10 +35,9 @@ mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
   console.log(`Connection to MongoDB ${mongoose.connection.name}`)
 })
-app.use("/users", userRouter)
-app.use("/companies", companyRouter)
-app.use("/employees", employeeRouter)
-app.use("/employeeshifts", employeeShiftsRouter)
-
+app.use('/users', userRouter)
+app.use('/companies', companyRouter)
+app.use('/employees', employeeRouter)
+app.use('/employeeshifts', employeeShiftsRouter)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
