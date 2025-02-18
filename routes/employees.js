e22ab165d5a3497743b3a8d0515c8b121afd33c9
MShubar <pokeeeeepow@gmail.com>
2025-02-18 20:09:24 +0300

    done the backend

--- routes/employees.js
+++ routes/employees.js
@@ -4,7 +4,9 @@ const employeeController = require('../controllers/employees')
 const { verifyToken } = require('../middleware/jwt')
 const upload = require('../middleware/multerConfig')
 
-router.post('/new', upload.fields([{ name: 'image' }]),
+router.post(
+  '/new',
+  upload.fields([{ name: 'image' }]),
   employeeController.createEmployee
 )
 
@@ -15,7 +17,11 @@ router.post('/new', upload.fields([{ name: 'image' }]),
 // }, employeeController.createEmployee);
 router.get('/', verifyToken, employeeController.findAllEmployees)
 router.get('/:employeeId', verifyToken, employeeController.showEmployee)
-router.put('/:employeeId', upload.fields([{ name: 'image' }]), employeeController.editEmployee)
+router.put(
+  '/:employeeId',
+  upload.fields([{ name: 'image' }]),
+  employeeController.editEmployee
+)
 router.delete('/:employeeId', verifyToken, employeeController.deleteEmployee)
 
 module.exports = router
